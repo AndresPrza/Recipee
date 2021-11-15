69094b686311edde94f12de4bc19bb7e76c35aca
Andres <andresprza@gmail.com>
2021-11-15 16:13:17 -0700

    waa

--- proto-laravel/resources/views/inicio.blade.php
+++ proto-laravel/resources/views/inicio.blade.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MEMBER STUDIOS</title>
+
+    <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/hover.css')}}">
+
 </head>
+
 <body>
-    <h1>La página funciona a la perfección con localhost uwuwuwuwu</h1>
+
+    <h1>ACTUALIZADO</h1>
+
 </body>
 </html>
